84ade904d5e90095e11862e85287be48591992c1
mateop21 <114003887+mateop21@users.noreply.github.com>
2023-03-08 10:22:16 -0500

    work3/8

---
+++ warmup3:3/color.js
@@ -0,0 +1,33 @@
+const collection = {
+{
+    'title'; 'History of Rugby Union in Scotland'
+    'url'; 'https://en.wikipedia.org/wiki/History_of_rugby_union_in_Scotland'
+    'date accessed'; 'March 2nd 2023'
+}
+
+{
+    'title'; 'Glenalmond College'
+    'url'; 'https://en.wikipedia.org/wiki/Glenalmond_College'
+    'date accessed'; 'March 2nd 2023'
+}
+
+{
+    'title'; 'Robbie Coltrane'
+    'url'; 'https://en.wikipedia.org/wiki/Robbie_Coltrane'
+    'date accessed'; 'March 2nd 2023'
+}
+
+{
+    'title'; 'James Bond'
+    'url'; 'https://en.wikipedia.org/wiki/James_Bond'
+    'date accessed'; 'March 2nd 2023'
+
+}
+
+[
+    'title'; 'BMW Z8'
+    'url'; 'https://en.wikipedia.org/wiki/BMW_Z8'
+    'date accessed'; 'March 2nd 2023'
+]
+
+}
